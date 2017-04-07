3a5ccba95570f9cfe92549bd165e31599c9a85c3
Yunhong <smartcyh@hotmail.com>
2017-04-07 18:13:12 +0800

    ms1100 voc sensor custom library

---
+++ ms1100Lib/ms1100.cpp
@@ -0,0 +1,35 @@
+#include "ms1100.h"
+#include "cmath"
+
+
+/*
+	获取甲醛浓度
+*/
+double ms1100::getFormaldehydeConcentration(int sensorPin)
+{
+	//读取模拟输入量
+	int val = analogRead(sensorPin);
+	//转换成电压
+	double voltage = val * (5 / 1023);
+	//根据电压计算出气体浓度
+	double logTemp = (-2.631) + 1.528 * voltage + (-0.125) * voltage * voltage;
+	//返回浓度值
+	return pow(10, logTemp);
+}
+
+/*
+*	获取甲苯浓度
+*/
+double ms1100::getTolueneConcentration(int sensorPin)
+{
+	//读取模拟输入量
+	int val = analogRead(sensorPin);
+	//转换成电压
+	double voltage = val * (5 / 1023);
+	//根据电压计算出气体浓度
+	double logTemp = (-7.071) + 2.852 * voltage + (-0.210) * voltage * voltage;
+	//返回浓度值
+	return pow(10, logTemp);
+}
+
+
